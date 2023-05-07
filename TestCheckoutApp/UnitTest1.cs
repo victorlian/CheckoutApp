ecf42d521436af8f1962c78463b597a2d750a5e1
DESKTOP-8BK4FU9\victo <victorlian97@gmail.com>
2023-05-08 00:12:19 +1200

    Initial commit

---
+++ TestCheckoutApp/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CheckoutApp;
+
+namespace TestCheckoutApp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
