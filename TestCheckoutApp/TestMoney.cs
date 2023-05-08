688c5c4a69b2abca8eb3125af1e95e5489133580
DESKTOP-8BK4FU9\victo <victorlian97@gmail.com>
2023-05-08 21:57:11 +1200

    [Red] Money addtion and subtraction

--- TestCheckoutApp/TestMoney.cs
+++ TestCheckoutApp/TestMoney.cs
@@ -9,7 +9,19 @@ namespace TestCheckoutApp
         [TestMethod]
         public void TestAddition()
         {
-            
+            Money m1 = new Money(1.23);
+            Money m2 = new Money(2.35);
+            Money m3 = new Money(3.58);
+            Assert.AreEqual(m3, m1 + m2);
+        }
+
+        [TestMethod]
+        public void TestSubtraction()
+        {
+            Money m1 = new Money(1.99);
+            Money m2 = new Money(0.99);
+            Money m3 = new Money(1);
+            Assert.AreEqual(m3, m1 - m2);
         }
     }
 }
