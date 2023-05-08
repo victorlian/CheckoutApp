f5a4a49bcc339ed2a797c0e3f68635a0e3a9529f
DESKTOP-8BK4FU9\victo <victorlian97@gmail.com>
2023-05-08 23:39:03 +1200

    Red - Add test for scan item at checkout

--- ConsoleApp1/Checkout.cs
+++ ConsoleApp1/Checkout.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace CheckoutApp
 {
-    class Checkout
+    public class Checkout
     {
         
     }
---
+++ TestCheckoutApp/TestCheckout.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CheckoutApp;
+
+namespace TestCheckoutApp
+{
+    [TestClass]
+    public class TestCheckOut
+    {
+        [TestMethod]
+        public void TestCanCreateOrderUponFirstScan()
+        {
+            Checkout c = new Checkout();
+            
+            Product item = new Product('p', 1.99);
+            c.ScanItem(item);
+
+            Assert.IsNotNull(c.Order);
+            Assert.AreEqual(1, c.Order.Count());
+        }
+    }
+}
