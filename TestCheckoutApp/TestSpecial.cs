eba671b832ccf22b25181ee5ffaa3436f7335ff1
DESKTOP-8BK4FU9\victo <victorlian97@gmail.com>
2023-05-09 21:50:01 +1200

    RED - Add test case for simple special

---
+++ TestCheckoutApp/TestSpecial.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using CheckoutApp;
+
+namespace TestCheckoutApp
+{
+    [TestClass]
+    public class TestSpecial
+    {
+        [TestMethod]
+        public void TestAmountOffSpecial()
+        {
+            AmountOffSpecial s = new AmountOffSpecial(new Money(0.98);
+
+            Product item = new Product("p", new Money(1.99));
+
+            Checkout c = new Checkout();
+            c.setSpecial(s)
+            c.ScanItem(item);
+
+            Order o = c.Order;
+            Assert.AreEqual(new Money(1.01), o.GetTotal());
+        }
+    }
+}
