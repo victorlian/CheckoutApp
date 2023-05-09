26de6f78a83289cd0e3be410c69a617b553dcf75
DESKTOP-8BK4FU9\victo <victorlian97@gmail.com>
2023-05-10 00:42:38 +1200

    RED - Add money percentage test

--- TestCheckoutApp/TestMoney.cs
+++ TestCheckoutApp/TestMoney.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CheckoutApp;
+using System.Collections.Generic;
 
 namespace TestCheckoutApp
 {
@@ -22,6 +23,18 @@ namespace TestCheckoutApp
             Money m2 = new Money(0.99);
             Money m3 = new Money(1);
             Assert.AreEqual(m3, m1 - m2);
+        }
+
+        [DataRow(1.99, 33, 1.33)]
+        [DataRow(1.01, 50, 0.51)] // Round up
+        [DataRow(1.99, 100, 0)]
+        [DataRow(1.99, 0, 1.99)]
+        public void TestPercentageOff(double original, int percentageOff, double result)
+        {
+            Money og = new Money(original);
+            Money res = og.PercentageOff(percentageOff);
+
+            Assert.AreEqual(result, res.Value);
         }
     }
 }
