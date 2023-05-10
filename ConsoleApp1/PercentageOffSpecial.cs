67283c4a30c71580582870fc2774a8c607f8db65
DESKTOP-8BK4FU9\victo <victorlian97@gmail.com>
2023-05-10 23:54:27 +1200

    green - implement percentage off special

---
+++ ConsoleApp1/PercentageOffSpecial.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckoutApp
+{
+    public class PercentageOffSpecial : ISpecial
+    {
+        public PercentageOffSpecial (int percentageOff, Product product)
+        {
+            PercentageOff = percentageOff;
+            Product = product;
+        }
+        public int PercentageOff { get; }
+
+        public Product Product { get; set; }
+
+        public void ApplySpecial (LineItem l)
+        {
+            if (l.Product == Product)
+            {
+                Money resultPrice = l.Product.Price.PercentageOff(PercentageOff);
+                Money priceSaved = l.Product.Price - resultPrice;
+                l.Discount += priceSaved;
+                l.PriceAfterDiscount -= priceSaved;
+            }
+        }
+    }
+}
